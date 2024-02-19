30ae8b75a30286474839192dd6416d4a52467d7a
unknown <you@example.com>
2024-02-19 07:30:34 +0600

    project add

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementsByClassName("button-click")
+console.log(button, );
+for (let btn = 0; index < array.length; index++) {
+ const element = btn[index]; 
+ console.log(element);                 
+}
